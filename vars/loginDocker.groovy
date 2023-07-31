9580258ad6c0403cc8503822af0bd02247044cc4
Abdullah Jimoh <jimoh.abdullah69@gmail.com>
2023-07-31 01:40:31 +0100

    initial commit

---
+++ vars/loginDocker.groovy
@@ -0,0 +1,7 @@
+#!usr/bin/env groovy
+
+def call() {
+        withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            sh "echo $PASS | docker login -u $USER --password-stdin"
+        }
+}
